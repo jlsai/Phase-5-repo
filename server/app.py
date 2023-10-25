c19329d6f2f12fa8510ec1bf8db2dd6a092377e2
jlsai <jon.lea@outlook.com>
2023-10-24 22:19:11 -0400

    Add init files, Create Models

--- server/app.py
+++ server/app.py
@@ -3,21 +3,76 @@
 # Standard library imports
 
 # Remote library imports
-from flask import request
-from flask_restful import Resource
+from config import db
+from flask_restful import Api, Resource
+from flask_migrate import Migrate
+from flask import Flask, make_response, jsonify, request, session
+import requests
+import os
+from flask_bcrypt import Bcrypt
+from faker import Faker
+import tmdbsimple as tmdb
+from models import Movie
 
-# Local imports
-from config import app, db, api
-# Add your model imports
+tmdb.API_KEY = 'c9ec267ab1d062779039d92435621a6b'
 
 
-# Views go here!
 
+
+# Create a Flask application
+app = Flask(__name__)
+BASE_DIR = os.path.abspath(os.path.dirname(__file__))
+DATABASE = os.environ.get(
+    "DB_URI", f"sqlite:///{os.path.join(BASE_DIR, 'app.db')}")
+
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = DATABASE
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.json.compact = False
+app.secret_key = b'Y\xf1Xz\x00\xad|eQ\x80t \xca\x1a\x10K'
+migrate = Migrate(app, db)
+
+db.init_app(app)
+
+bcrypt = Bcrypt(app)
+
+api = Api(app)
+
+
+# API endpoint URL with API key
+api_key = 'c9ec267ab1d062779039d92435621a6b'
+api_url = 'https://api.themoviedb.org/3/discover/movie?api_key=' + api_key  # Replace with the actual API endpoint
+
+# Define your routes
 @app.route('/')
 def index():
     return '<h1>Phase 4 Project Server</h1>'
 
+@app.route('/data')
+def get_data():
+    # Set up the headers with your API key
+    headers = {
+        'Authorization': 'Bearer c9ec267ab1d062779039d92435621a6b'
+    }
+
+    # Send a GET request to the API with your API key
+    response = requests.get(api_url, headers=headers)
+    
+    # Check if the request was successful
+    if response.status_code == 200:
+        data = response.json()
+        return jsonify(data)
+    else:
+        return jsonify({"error": "Failed to fetch data from the API"}), 500
+    
+class Movies(Resource):
+    def get(self):
+        movies = [movie.to_dict() for movie in Movies.query.all()]
+        return make_response(movies, 200)
+
+api.add_resource(Movies, "/movies")  
 
 if __name__ == '__main__':
+    # Run the Flask app on port 5555
     app.run(port=5555, debug=True)
-
--- server/seed.py
+++ server/seed.py
@@ -7,11 +7,43 @@ from random import randint, choice as rc
 from faker import Faker
 
 # Local imports
-from app import app
+import requests
+from app import app, api_url
 from models import db
+from models import User, Movie, Rating, Comment, MovieList, Log
+
+def create_movies():
+    headers = {
+        'Authorization': 'Bearer c9ec267ab1d062779039d92435621a6b'
+    }
+        # Send a GET request to the API to fetch the first 10 movies
+    response = requests.get(api_url, headers=headers)
+    data = response.json()
+    movies_to_add = data.get("results", [])[:10]  # Get the first 10 movies from the API response
+
+            # Iterate through the movies and add them to the database
+    for movie_data in movies_to_add:
+        title = movie_data.get("title")
+        vote_average = movie_data.get("vote_average")
+        image_url = movie_data.get("poster_path")
+        movie = Movie(
+            title=title, 
+            rating=vote_average,
+            img_url=image_url
+            )
+        db.session.add(movie)    
+        db.session.commit()
+        print(f"Added movie: {title}")
+    db.session.commit()
+
 
 if __name__ == '__main__':
     fake = Faker()
     with app.app_context():
         print("Starting seed...")
-        # Seed code goes here!
+
+        db.create_all()  # Create the database tables if they don't exist
+        create_movies()   # Call the function to add movies from the API
+        db.session.commit()  # Commit changes to the database
+        
+        print("Done seeding!")
