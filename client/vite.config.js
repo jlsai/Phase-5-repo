393e7a1eab3c848ad461b4804c70d0efbb63d2b3
jlsai <jon.lea@outlook.com>
2023-11-08 03:58:17 -0500

    Add Deployment

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,6 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
+    base: '/',
     // changes our vite to launch out of port 3000
     port: 3000,
     // this allows the app to be accessed from outside the localhost 
